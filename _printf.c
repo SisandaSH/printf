07bd0136f34abb3df130f8d8ce37ca4a165e9441
SisandaSH <ntombifuthishwala1978@gmail.com>
2023-10-22 13:52:08 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,127 +1,53 @@
-#include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
+#include <stdio.h>
 
-/**
- * _printf - Custom printf function
- * @format: The format string
- *
- * Return: the number of characters printed (excluding the null byte)
- */
-int _printf(const char *format, ...)
-{
+#define BUFF_SIZE 1024
+
+void print_buffer(char *buffer, int *index) {
+    if (*index > 0) {
+        fwrite(buffer, sizeof(char), *index, stdout);
+        *index = 0;
+    }
+}
+
+void _printf(const char *format, ...) {
     va_list args;
     va_start(args, format);
 
-    int printed_chars = 0;
-    int buffer_index = 0;
     char buffer[BUFF_SIZE];
+    int buffer_index = 0;
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            buffer[buffer_index++] = *format;
-            if (buffer_index == BUFF_SIZE - 1)
-            {
-                write(1, buffer, buffer_index);
-                printed_chars += buffer_index;
-                buffer_index = 0;
-            }
-            else
-            {
-                printed_chars++;
-            }
-        }
-        else
-        {
-            format++; /* Move past '%' */
-            if (*format == '\0')
-                break;
-
-            switch (*format)
-            {
-                case 'c':
-                {
-                    char c = va_arg(args, int);
-                    buffer[buffer_index++] = c;
-                    if (buffer_index == BUFF_SIZE - 1)
-                    {
-                        write(1, buffer, buffer_index);
-                        printed_chars += buffer_index;
-                        buffer_index = 0;
-                    }
-                    else
-                    {
-                        printed_chars++;
-                    }
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            switch (*format) {
+                case 'd':
+                case 'i': {
+                    int i = va_arg(args, int);
+                    buffer_index += snprintf(buffer + buffer_index, BUFF_SIZE - buffer_index, "%d", i);
                     break;
                 }
-
-                case 's':
-                {
-                    char *str = va_arg(args, char *);
-                    if (str == NULL)
-                        str = "(null)";
-
-                    while (*str)
-                    {
-                        buffer[buffer_index++] = *str;
-                        if (buffer_index == BUFF_SIZE - 1)
-                        {
-                            write(1, buffer, buffer_index);
-                            printed_chars += buffer_index;
-                            buffer_index = 0;
-                        }
-                        else
-                        {
-                            printed_chars++;
-                        }
-                        str++;
-                    }
+                case 's': {
+                    char *s = va_arg(args, char*);
+                    buffer_index += snprintf(buffer + buffer_index, BUFF_SIZE - buffer_index, "%s", s);
                     break;
                 }
-
-                case '%':
-                {
-                    buffer[buffer_index++] = '%';
-                    if (buffer_index == BUFF_SIZE - 1)
-                    {
-                        write(1, buffer, buffer_index);
-                        printed_chars += buffer_index;
-                        buffer_index = 0;
-                    }
-                    else
-                    {
-                        printed_chars++;
-                    }
-                    break;
-                }
-
                 default:
-                {
                     buffer[buffer_index++] = '%';
                     buffer[buffer_index++] = *format;
-                    if (buffer_index >= BUFF_SIZE - 2)
-                    {
-                        write(1, buffer, buffer_index);
-                        printed_chars += buffer_index;
-                        buffer_index = 0;
-                    }
-                    else
-                    {
-                        printed_chars += 2;
-                    }
-                }
             }
+        } else {
+            buffer[buffer_index++] = *format;
         }
+
+        if (buffer_index >= BUFF_SIZE) {
+            print_buffer(buffer, &buffer_index);
+        }
+
         format++;
     }
 
-    write(1, buffer, buffer_index);
-    printed_chars += buffer_index;
+    print_buffer(buffer, &buffer_index);
 
     va_end(args);
-
-    return printed_chars;
 }
